c71d8eca62145f89e10a87fade6a51046ea67a43
Bhoumik Shah <84918082+BhoumikShah@users.noreply.github.com>
2024-03-12 12:36:47 +0530

    Add files via upload

---
+++ Sandbox.java
@@ -0,0 +1,14 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+    System.out.println("Hello,World");
+    int second = 32;
+int first = 25;
+second = 17;
+first = second;
+
+System.out.println(second);
+    }
+}
