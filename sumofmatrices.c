2f8cc6545cd82d26df14ada6d3b280370d6f14e1
Bhoumik Shah <84918082+BhoumikShah@users.noreply.github.com>
2025-07-11 11:09:36 +0530

    Update sumofmatrices.c

--- sumofmatrices.c
+++ sumofmatrices.c
@@ -22,7 +22,13 @@ void add(int a[2][2], int b[2][2], int result[2][2]) {
 int main() {
     // Correct initialization for a 2x2 array
     int matrix1[2][2] = {{10, 20}, {30, 40}};
+    printf("First Matrix:\n");
+    display(matrix1);
+
     int matrix2[2][2] = {{1, 2}, {3, 4}};
+    printf("Second Matrix:\n");
+    display(matrix2);
+
     int sum[2][2];
 
     add(matrix1, matrix2, sum);
