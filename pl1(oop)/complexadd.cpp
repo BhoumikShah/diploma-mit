142a3384fd8f3f64b5385d899d5df7bfacc5b56d
Bhoumik Shah <84918082+BhoumikShah@users.noreply.github.com>
2025-08-06 12:15:04 +0530

    Update complexadd.cpp

--- pl1(oop)/complexadd.cpp
+++ pl1(oop)/complexadd.cpp
@@ -1,3 +1,4 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
@@ -6,6 +7,9 @@ private:
     int real, imag;
 
 public:
+    number() : real(0), imag(0) {}
+    number(int r, int i) : real(r), imag(i) {}
+
     void read() {
         cout << "Enter real part: ";
         cin >> real;
@@ -13,37 +17,44 @@ public:
         cin >> imag;
     }
 
-    void add(number c1, number c2) {
-        real = c1.real + c2.real;
-        imag = c1.imag + c2.imag;
-    }
-
-    void subtract(number c1, number c2) {
-        real = c1.real - c2.real;
-        imag = c1.imag - c2.imag;
-    }
-
-    void multiply(number c1, number c2) {
-        real = (c1.real * c2.real) - (c1.imag * c2.imag);
-        imag = (c1.real * c2.imag) + (c1.imag * c2.real);
-    }
-
-    void divide(number c1, number c2) {
-        int denominator = (c2.real * c2.real) + (c2.imag * c2.imag);
-        if (denominator == 0) {
-            cout << "Division by zero not possible!" << endl;
-            real = imag = 0;
-            return;
-        }
-        real = (c1.real * c2.real + c1.imag * c2.imag) / denominator;
-        imag = (c1.imag * c2.real - c1.real * c2.imag) / denominator;
-    }
-
-    void display(string operation) {
+    void display(string operation) const {
         cout << operation << ": " << real 
              << (imag >= 0 ? "+" : "") << imag << "i" << endl;
     }
+
+    // Friend functions for operator overloading
+    friend number operator+(const number& n1, const number& n2);
+    friend number operator-(const number& n1, const number& n2);
+    friend number operator*(const number& n1, const number& n2);
+    friend number operator/(const number& n1, const number& n2);
 };
+
+// Addition
+number operator+(const number& n1, const number& n2) {
+    return number(n1.real + n2.real, n1.imag + n2.imag);
+}
+
+// Subtraction
+number operator-(const number& n1, const number& n2) {
+    return number(n1.real - n2.real, n1.imag - n2.imag);
+}
+
+// Multiplication
+number operator*(const number& n1, const number& n2) {
+    return number((n1.real * n2.real) - (n1.imag * n2.imag),
+                  (n1.real * n2.imag) + (n1.imag * n2.real));
+}
+
+// Division
+number operator/(const number& n1, const number& n2) {
+    int denominator = (n2.real * n2.real) + (n2.imag * n2.imag);
+    if (denominator == 0) {
+        cout << "Division by zero not possible!" << endl;
+        return number(0, 0);
+    }
+    return number((n1.real * n2.real + n1.imag * n2.imag) / denominator,
+                  (n1.imag * n2.real - n1.real * n2.imag) / denominator);
+}
 
 int main() {
     number c1, c2, result;
@@ -52,17 +63,10 @@ int main() {
     cout << "Enter details of number2" << endl;
     c2.read();
 
-    result.add(c1, c2);
-    result.display("Addition");
-
-    result.subtract(c1, c2);
-    result.display("Subtraction");
-
-    result.multiply(c1, c2);
-    result.display("Multiplication");
-
-    result.divide(c1, c2);
-    result.display("Division");
+    (c1 + c2).display("Addition");
+    (c1 - c2).display("Subtraction");
+    (c1 * c2).display("Multiplication");
+    (c1 / c2).display("Division");
 
     return 0;
 }
